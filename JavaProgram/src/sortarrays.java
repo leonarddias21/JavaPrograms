e3bbeff6458139222b0eb37f7866764c234a85a7
leona <leona@User>
2023-07-05 16:57:55 +0530

    sixth commit

---
+++ JavaProgram/src/sortarrays.java
@@ -0,0 +1,23 @@
+
+public class sortarrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] marksArray=new int[] {44,23,78,89};
+		int temp;
+		for(int i=0;i<marksArray.length;i++) {
+			for (int j=i;j<marksArray.length;j++) {
+				if(marksArray[i]>marksArray[j]) {
+					temp=marksArray[i];
+					marksArray[i]=marksArray[j];
+					marksArray[j]=temp;
+				}
+				
+			}
+		}
+		for(int a : marksArray) {
+			System.out.print(a);
+		}
+	}
+
+}
